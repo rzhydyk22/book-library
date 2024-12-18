591e3c7727157977e011c37d0be90864a6698384
rzhydyk22 <rzhydyk22@gmail.com>
2024-12-18 23:26:54 +0200

    Add main structure

---
+++ src/app/pages/home/book-dialog/book-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-dialog.component.html',
+  styleUrl: './book-dialog.component.scss'
+})
+export class BookDialogComponent {
+
+}
---
+++ src/app/pages/home/books-page/books-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-page.component.html',
+  styleUrl: './books-page.component.scss'
+})
+export class BooksPageComponent {
+
+}
